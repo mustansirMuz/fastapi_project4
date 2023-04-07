ae912dbbe8e31454235b65fad3f5b870e5ed1453
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-04-07 15:28:02 +0500

    change database back to postgresql hosted separately

--- database.py
+++ database.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://todos:OxVEHPm7ZtKrquKuxwLPGXH46GYDVhWY@dpg-cgnuu4l269v5rj8qpp40-a.singapore-postgres.render.com/todos_v42e"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
