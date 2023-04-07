aa6f44e3cd0d98a052a8c7e313708c9bdea47653
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-04-07 15:38:37 +0500

    edit todos route

--- routers/todos.py
+++ routers/todos.py
@@ -25,7 +25,7 @@ logging.basicConfig(
 )
 
 router = APIRouter(
-    prefix="/todos", tags=["todos"], responses={404: {"description": "Not found"}}
+    prefix="/", tags=["todos"], responses={404: {"description": "Not found"}}
 )
 
 models.Base.metadata.create_all(bind=engine)
