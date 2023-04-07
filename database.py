2fde95a5137a00d4f0795a11139c0d56105d3f9d
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-04-07 14:54:20 +0500

    use sqlite db

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql+psycopg2://mustansir:12345678@localhost/todos_project4"
-)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
